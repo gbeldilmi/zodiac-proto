5537ff2044b9e6178d5fadcbd59b791a6db3aeed
Guillaume BELDILMI <gbeldilmi@pm.me>
2023-03-26 21:59:40 +0200

    add service package info

---
+++ source/com/gbeldilmi/zodiac/service/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all the services subpackages. It's used to organize the different services hosted by the
+ * server into a common package.
+ * @author gbeldilmi
+ */
+package com.gbeldilmi.zodiac.service;
+
